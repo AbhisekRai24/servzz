cafcb15775c6e8c3bc95962aeb6d7dc5e3b09167
ahbisek <abhisek1999rai@gmail.com>
2025-05-19 14:28:42 +0545

    banner update

--- lib/view/home_page_view.dart
+++ lib/view/home_page_view.dart
@@ -50,11 +50,11 @@ class HomePageView extends StatelessWidget {
       ],
     ),
   ),
-  // appBar: AppBar(
-  //   backgroundColor: Colors.grey[200],
-  //   elevation: 0,
-  //   iconTheme: IconThemeData(color: Colors.black),
-  // ),
+  appBar: AppBar(
+    backgroundColor: Colors.grey[200],
+    elevation: 0,
+    iconTheme: IconThemeData(color: Colors.black),
+  ),
   body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
@@ -104,15 +104,15 @@ class HomePageView extends StatelessWidget {
                 SizedBox(height: 20),
 
                 // Banner
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Image.asset(
-                    "assets/image/food_banner.jpg", 
-                    fit: BoxFit.cover,
-                    height: 180,
-                    width: double.infinity,
-                  ),
-                ),
+                // ClipRRect(
+                //   borderRadius: BorderRadius.circular(20),
+                //   child: Image.asset(
+                //     "assets/image/food_banner.jpg", 
+                //     fit: BoxFit.cover,
+                //     height: 180,
+                //     width: double.infinity,
+                //   ),
+                // ),
                 SizedBox(height: 20),
 
                 // Cafes section
