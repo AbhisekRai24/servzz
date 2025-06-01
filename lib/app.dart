84334c469d35c21b20046f5bf553436999ecab01
ahbisek <abhisek1999rai@gmail.com>
2025-06-01 17:30:16 +0545

    Created the theme
    # Conflicts:
    #	lib/assets/image/berry_cheesecake.jpg
    #	lib/assets/image/food.jpg
    #	lib/assets/image/servzz_logo.png

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 // Material app
 
 import 'package:flutter/material.dart';
+import 'package:servzz/themes/theme_Data.dart';
 import 'package:servzz/view/home_page_view.dart';
 import 'package:servzz/view/login_view.dart';
 import 'package:servzz/view/splash_screen.dart';
@@ -11,8 +12,18 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      
+      // main theme of the app 
+      // theme as in the customizable themes in androids
+      theme: getApplicationTheme(),
+      title: "Employee",
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => const HomePageView(),
+        // '/employee': (context) => const EmployeeView(),
+        // '/table': (context) => const HomePage(),
+        // '/bottom': (context) => const DashboardScreenView(),
+      },
     );
   }
-}
+} 
