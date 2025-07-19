9d59022c7b0d19b1cadbd10a3df06ae9760857ac
ahbisek <abhisek1999rai@gmail.com>
2025-07-20 00:02:46 +0545

    cart state

---
+++ lib/features/cart/presentation/view_model/cart_state.dart
@@ -0,0 +1,22 @@
+import 'package:equatable/equatable.dart';
+import 'package:servzz/features/cart/domain/model/cart_item.dart';
+
+
+class CartState extends Equatable {
+  final List<CartItem> items;
+
+  const CartState({this.items = const []});
+
+  double get totalPrice =>
+      items.fold(0, (sum, item) => sum + item.totalPrice);
+
+  int get totalItems =>
+      items.fold(0, (sum, item) => sum + item.quantity);
+
+  CartState copyWith({List<CartItem>? items}) {
+    return CartState(items: items ?? this.items);
+  }
+
+  @override
+  List<Object?> get props => [items];
+}
