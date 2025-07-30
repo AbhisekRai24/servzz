45ae9371c5f1c3d2971a27343115c50131962cec
ahbisek <abhisek1999rai@gmail.com>
2025-07-30 23:23:26 +0545

    new endpoints

--- lib/app/constant/api_endpoints.dart
+++ lib/app/constant/api_endpoints.dart
@@ -5,7 +5,8 @@ class ApiEndpoints {
   static const connectionTimeout = Duration(seconds: 1000);
   static const receiveTimeout = Duration(seconds: 1000);
 
-  static const String serverAddress = "http://10.0.2.2:5050";
+  // static const String serverAddress = "http://10.0.2.2:5050";
+  static const String serverAddress = "http://192.168.1.108:5050";
 
   // For iPhone (uncomment if needed)
   static const String baseUrl = "$serverAddress/api/";
